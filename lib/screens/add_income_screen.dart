5a464f7c984cac9714cd651087d9709946f9371e
Sunny Jaiswal <sunnyjaiswaldelhi@gmail.com>
2021-07-06 20:03:42 +0530

    Megred form inside add income screen. Added frequency drop down

--- lib/screens/add_income_screen.dart
+++ lib/screens/add_income_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
-class AddIncomeScreen extends StatelessWidget {
+class AddIncomeScreen extends StatefulWidget {
   const AddIncomeScreen({Key? key}) : super(key: key);
   static final routeName = 'add-income-screen';
+
+  @override
+  _AddIncomeScreenState createState() => _AddIncomeScreenState();
+}
+
+class _AddIncomeScreenState extends State<AddIncomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String frequency = 'Monthly';
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +20,57 @@ class AddIncomeScreen extends StatelessWidget {
           child: Text('FinSim - Add Income'),
         ),
       ),
-      body: null,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Income Name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter name of the income";
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                children: [
+                  Text('Frequency'),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  DropdownButton<String>(
+                    value: frequency,
+                    icon: const Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    underline: Container(
+                      height: 2,
+                      color: Theme.of(context).accentColor,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        frequency = newValue!;
+                      });
+                    },
+                    items: <String>['Monthly', 'Yearly']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
