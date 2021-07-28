bc00a6044ea670f847c268162aeda5ba66b0a631
Sunny Jaiswal <sunnyjaiswaldelhi@gmail.com>
2021-07-28 17:26:23 +0530

    UI changes in Drawer

--- lib/widgets/navigation_drawer.dart
+++ lib/widgets/navigation_drawer.dart
@@ -9,42 +9,63 @@ class NavigationDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-            child: Image.asset('assets/images/logo_white.png'),
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.4,
+      child: Drawer(
+        child: Container(
+          decoration:
+              new BoxDecoration(color: Theme.of(context).primaryColorDark),
+          child: ListView(
+            children: [
+              Container(
+                height: 80,
+                child: DrawerHeader(
+                  decoration:
+                      BoxDecoration(color: Theme.of(context).primaryColorDark),
+                  child: Image.asset('assets/images/logo_white.png'),
+                ),
+              ),
+              ListTile(
+                title: Text('Home', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, HomeScreen.routeName);
+                },
+              ),
+              ListTile(
+                title: Text('Income', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, IncomeSourcesScreen.routeName);
+                },
+              ),
+              ListTile(
+                title:
+                    Text('Expenditure', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(
+                      context, ExpenditureSourcesScreen.routeName);
+                },
+              ),
+              ListTile(
+                title: Text('Assets', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, InvestmentListScreen.routeName);
+                },
+              ),
+              ListTile(
+                title:
+                    Text('Liabilities', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, InvestmentListScreen.routeName);
+                },
+              ),
+            ],
           ),
-          ListTile(
-            title: Text('Home'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, HomeScreen.routeName);
-            },
-          ),
-          ListTile(
-            title: Text('Current Income Sources'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, IncomeSourcesScreen.routeName);
-            },
-          ),
-          ListTile(
-            title: Text('Current Expenditure Sources'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, ExpenditureSourcesScreen.routeName);
-            },
-          ),
-          ListTile(
-            title: Text('Investments'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, InvestmentListScreen.routeName);
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
